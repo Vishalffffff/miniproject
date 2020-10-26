e28b68f6b65a86cee85842da092faffefbfe0d24
Vishal <drive.vishal.f@gmail.com>
2020-10-26 12:17:13 +0530

    Commit at 12:16 on 26th October 2020

--- server.js
+++ server.js
@@ -33,11 +33,33 @@ client.connect()
              
           })
           .then((results)=>{
-              console.log('results?',results) ;
+             // console.log('results?',results) ;
             res.render('hostel',results);
           });
         
 }) ;
+app.get('/',(req,res)=>{
+    const client = new Client({
+        user:'postgres',
+        host:'localhost',
+        database:'hostel',
+        password:'7408199514',
+        port:5432,
+
+    });
+    
+client.connect()
+          .then(()=>{
+              return client.query('SELECT * FROM pg');
+             
+          })
+          .then((results)=>{
+             // console.log('results?',results) ;
+            res.render('home',results);
+          });
+        
+}) ;
+
 
 app.get('/search',(req,res)=>{
     res.render('search'); 
@@ -62,8 +84,8 @@ app.post('/pg/add',(req,res)=>{
 client.connect()
           .then(()=>{
               console.log('DATABASE CONNECT SUCCESS') ;
-              const sql = 'INSERT INTO pg (name,description,pricing,contactno,fooditems,rating) VALUES ($1, $2, $3, $4, $5, $6)' ;
-              const params = [req.body.name,req.body.description,req.body.pricing,req.body.contactno,req.body.fooditems,req.body.rating] ;
+              const sql = 'INSERT INTO pg (name,description,pricing,contactno,fooditems,rating,colleges,image1,image2,image3) VALUES ($1, $2, $3, $4, $5, $6,$7,$8,$9,$10,$11)' ;
+              const params = [req.body.name,req.body.description,req.body.pricing,req.body.contactno,req.body.fooditems,req.body.rating,req.body.colleges,req.body.image1,req.body.image2,req.body.image3] ;
               return client.query(sql,params) ;
           })
           .then((results)=>{
@@ -123,6 +145,30 @@ app.get('/hostel/edit/:id',(req,res)=>{
               });
     
             })
+app.get('/view/:id',(req,res)=>{
+                const client = new Client({
+                    user:'postgres',
+                    host:'localhost',
+                    database:'hostel',
+                    password:'7408199514',
+                    port:5432,
+        
+                 });
+            
+            client.connect()
+                  .then(()=>{
+                      const sql = 'SELECT * FROM pg WHERE id=$1'
+                      const params = [req.params.id] ;
+                      return client.query(sql,params) ;
+                  })
+                  .then((results)=>{
+                      //console.log('results?',results) ;
+                     res.render('view',{view:results.rows[0]})  ;
+                     
+                  });
+        
+                });
+
 app.post('/hostel-edit/:id',(req,res)=>{
 
     const client = new Client({
@@ -136,8 +182,8 @@ app.post('/hostel-edit/:id',(req,res)=>{
 
 client.connect()
       .then(()=>{
-          const sql = 'UPDATE pg SET name=$1, description=$2,pricing=$3,contactno=$4,fooditems=$5,rating=$6 WHERE id=$7'
-          const params = [req.body.name,req.body.description,req.body.pricing,req.body.contactno,req.body.fooditems,req.body.rating,req.params.id] ;
+          const sql = 'UPDATE pg SET name=$1, description=$2,pricing=$3,contactno=$4,fooditems=$5,rating=$6,colleges=$7,image1=$8,image2=$9,image3=$10 WHERE id=$11'
+          const params = [req.body.name,req.body.description,req.body.pricing,req.body.contactno,req.body.fooditems,req.body.rating,req.body.colleges,req.body.image1,req.body.image2,req.body.image3,req.params.id] ;
           return client.query(sql,params) ;
       })
       .then((results)=>{
